46b5fea091abc85d89ed3a8433ec05debfe97e0c
Romero <aromeroarias7@gmail.com>
2023-07-10 15:47:34 -0500

    Optimized structure

--- Backend/appCAH/views.py
+++ Backend/appCAH/views.py
@@ -13,14 +13,50 @@ def login(request):
 def registrarse(request):
   return render(request, 'login/registrarse.html')
 
-def oferta(request):
-  return render(request, 'Oferta/oferta.html')
+def agricultor(request):
+  return render(request, 'Vehiculo/agricultor.html')
 
-def registrarOferta(request):
-  return render(request, 'Oferta/registrar-oferta.html')
+def registrarAgricultor(request):
+  return render(request, 'Agricultor/registrar-agricultor.html')
 
-def modificarOferta(request):
-  return render(request, 'Oferta/modificar-oferta.html')
+def modificarAgricultor(request):
+  return render(request, 'Agricultor/modificar-agricultor.html')
+
+def mayorista(request):
+  return render(request, 'Mayorista/mayorista.html')
+
+def registrarMayorista(request):
+  return render(request, 'Mayorista/registrar-mayorista.html')
+
+def modificarMayorista(request):
+  return render(request, 'Mayorista/modificar-mayorista.html')
+
+def conductor(request):
+  return render(request, 'Conductor/conductor.html')
+
+def registrarConductor(request):
+  return render(request, 'Conductor/registrar-conductor.html')
+
+def modificarConductor(request):
+  return render(request, 'Conductor/modificar-conductor.html')
+
+def bodega(request):
+  return render(request, 'Bodega/bodega.html')
+
+def registrarBodega(request):
+  return render(request, 'Bodega/registrar-bodega.html')
+
+def modificarBodega(request):
+  return render(request, 'Bodega/modificar-bodega.html')
+
+def producto(request):
+  return render(request, 'Producto/producto.html')
+
+def registrarProducto(request):
+  return render(request, 'Producto/registrar-producto.html')
+
+def modificarProducto(request):
+  return render(request, 'Producto/modificar-producto.html')
 
 def vehiculo(request):
   return render(request, 'Vehiculo/vehiculo.html')
@@ -30,3 +66,14 @@ def registrarVehiculo(request):
 
 def modificarVehiculo(request):
   return render(request, 'Vehiculo/modificar-vehiculo.html')
+
+def oferta(request):
+  return render(request, 'Oferta/oferta.html')
+
+def registrarOferta(request):
+  return render(request, 'Oferta/registrar-oferta.html')
+
+def modificarOferta(request):
+  return render(request, 'Oferta/modificar-oferta.html')
+
+
