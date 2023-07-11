d27a8fc9efce21de79ebc8260497529e7ed83c01
Romero <aromeroarias7@gmail.com>
2023-07-11 15:21:18 -0500

    Registros and login ready

--- Backend/appCAH/static/index.js
+++ Backend/appCAH/static/index.js
@@ -1,11 +1,9 @@
 // import { App } from "../../../Frontend/App.js";
 import { Modal } from "./Components/Modal.js";
+import { Menu } from "./Components/Menu.js";
 
 const d = document,
   $main = d.querySelector("main");
-
-// // ** Eventos globales ------------------------------------------------------------------
-// d.addEventListener("DOMContentLoaded", App);
 
 // ** Delegacion de eventos, click-----------------------------------------------------
 d.addEventListener("click", (e) => {
@@ -28,5 +26,23 @@ d.addEventListener("click", (e) => {
   //Para cerrar modal
   if (e.target.matches(".modal .cancelar-modal")) {
     d.querySelector(".modal").remove();
+  }
+
+  //Para cerrar sesion
+  if (e.target.matches(".cerrar-sesion")) {
+    alert("Sesion Cerrada");
+    d.cookie = "user_id =; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+
+  //Para abrir menu
+  if (e.target.matches(".click-menu")) {
+    const $active = d.querySelector(".menu-despegable");
+    if ($active) {
+      $active.remove();
+    } else {
+      const $mainIco = d.querySelector(".menu");
+      $mainIco.classList.add("active");
+      $mainIco.appendChild(Menu());
+    }
   }
 });
